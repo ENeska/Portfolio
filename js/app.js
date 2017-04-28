bf848618831e0d0ba9e3ff6d5bae8416f69f3f60
Elżbieta Neska <ela.neska@wp.pl>
2017-04-28 15:49:07 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,46 +1,53 @@
 
-$('.js_btn_menu').click(function() {
-    $(this).toggleClass('active');
-    $('#js_nav').toggleClass('open');
-});
-
-$('nav').find('a').click(function() {
-    $('.js_btn_menu').removeClass('active');
-    $('#js_nav').removeClass('open');
-});
+$(document).ready(function() {
 
 
-$('a[href*="#"]').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
+    $('.js_btn_menu').click(function() {
+        $(this).toggleClass('active');
+        $('#js_nav').toggleClass('open');
+    });
+
+    $('nav').find('a').click(function() {
+        $('.js_btn_menu').removeClass('active');
+        $('#js_nav').removeClass('open');
+    });
+
+
+    $('a[href*="#"]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    var h = window.innerHeight;
+
+    window.onscroll = function() {change_color()};
+
+    function change_color() {
+        var btn_menu = '.btn_menu span';
+        var body = $('body');
+
+        if (document.body.scrollTop >= h-75 || document.documentElement.scrollTop >= h-75) {
+
+            body.find(btn_menu).addClass('color_dark');
+            body.find('.btn_menu').addClass('color');
+
+        } else {
+            body.find(btn_menu).removeClass('color_dark');
+            body.find('.btn_menu').removeClass('color');
+
         }
     }
+    
 });
 
-var h = window.innerHeight;
 
-window.onscroll = function() {change_color()};
-
-function change_color() {
-    var btn_menu = '.btn_menu span';
-    var body = $('body');
-
-    if (document.body.scrollTop >= h-75 || document.documentElement.scrollTop >= h-75) {
-
-        body.find(btn_menu).addClass('color_dark');
-        body.find('.btn_menu').addClass('color');
-
-    } else {
-        body.find(btn_menu).removeClass('color_dark');
-        body.find('.btn_menu').removeClass('color');
-
-    }
-}
 
 
